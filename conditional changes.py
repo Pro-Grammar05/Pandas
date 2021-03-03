805ef853ca8b08e70bbe1efaa6aa414170b0f288
Adithya <53051177+Pro-Grammar05@users.noreply.github.com>
2021-03-03 09:05:45 +0530

    Update conditional changes.py

--- conditional changes.py
+++ conditional changes.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-df = pd.read_csv('D:\Chamber of Secrets\Python\CSV files\pokemon_data.csv')
+df = pd.read_csv('pokemon_data.csv')
 
 # change fire types to flamer types
 df.loc[df['Type 1'] == 'Fire', 'Type 1'] = 'Flamer'
